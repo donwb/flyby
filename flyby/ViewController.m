1738bb90d7a489c2c958bc8bb722b5f601c3c36b
don bronwing <don.browning@gmail.com>
2014-04-05 11:19:06 -0400

    stubbed out estimate stuff

--- flyby/ViewController.m
+++ flyby/ViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "ESTBeaconManager.h"
 
-@interface ViewController ()
+
+@interface ViewController () <ESTBeaconManagerDelegate>
+
+@property (nonatomic, strong) ESTBeacon         *beacon;
+@property (nonatomic, strong) ESTBeaconManager  *beaconManager;
+@property (nonatomic, strong) ESTBeaconRegion   *beaconRegion;
+
+@property (nonatomic, strong) UISwitch          *enterRegionSwitch;
+@property (nonatomic, strong) UISwitch          *exitRegionSwitch;
+
 
 @end
 
@@ -18,6 +28,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.beaconManager = [[ESTBeaconManager alloc]init];
+    self.beaconManager.delegate = self;
+    self.beaconManager.avoidUnknownStateBeacons = YES;
+    NSUUID *beaconId = [[NSUUID alloc]initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"];
+    
+    ESTBeaconRegion *region = [[ESTBeaconRegion alloc] initWithProximityUUID:beaconId identifier:@"MyRegion"];
+    
+    [self.beaconManager startMonitoringForRegion:region];
+    
+    [self.beaconManager stopMonitoringForRegion:region];
+    
 }
 
 - (void)didReceiveMemoryWarning
