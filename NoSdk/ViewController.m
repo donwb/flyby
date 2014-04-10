3d873433c7c17bed00470ff52eea2c67519af751
Don Browning <don.browning@gmail.com>
2014-04-10 15:52:23 -0400

    Initial Commit

---
+++ NoSdk/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NoSdk
+//
+//  Created by Don Browning on 4/10/14.
+//  Copyright (c) 2014 Don Browning. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
