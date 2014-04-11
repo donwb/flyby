fde6949bc056c1768057ca99ec0446c40f598604
Don Browning <don.browning@gmail.com>
2014-04-11 15:07:20 -0400

    lots of UI tweaks

--- NoSdk/ViewController.m
+++ NoSdk/ViewController.m
@@ -7,17 +7,209 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+
+@property (weak, nonatomic) IBOutlet UIButton *enterButton;
+@property (weak, nonatomic) IBOutlet UIButton *exitButton;
+
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (weak, nonatomic) IBOutlet UITextField *locationTextField;
+@property (weak, nonatomic) IBOutlet UIView *locationContainer;
+
+@property CGRect initialAvatarFrame;
 
 @end
 
 @implementation ViewController
 
+CLLocationManager *locationManager;
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self roundAvatar:100.0];
+    self.initialAvatarFrame = self.avatarImageView.frame;
+    
+    
+    // outside until proven inside
+    [self outside];
+    
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.delegate = self;
+    
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"];
+    CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:@"com.donwb.beacon.region"];
+    region.notifyEntryStateOnDisplay = YES;
+    region.notifyOnEntry = YES;
+    region.notifyOnExit = YES;
+    
+    [self.locationManager startMonitoringForRegion:region];
+    [self.locationManager requestStateForRegion:region];
+    
+}
+
+#pragma mark BLE Stuff
+
+-(void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region {
+    
+    NSLog(@"Determining state....");
+    
+    if(state == CLRegionStateInside) {
+        NSLog(@"State Inside....");
+        
+        CLBeaconRegion *beaconRegion = (CLBeaconRegion *) region;
+        [self.locationManager startRangingBeaconsInRegion:beaconRegion];
+        
+        [self inside];
+    }
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    NSLog(@"Did enter region");
+    
+    if([region isKindOfClass:[CLBeaconRegion class]]) {
+        CLBeaconRegion *beaconRegion = (CLBeaconRegion *) region;
+        
+        if([beaconRegion.identifier isEqualToString:@"com.donwb.beacon.region"]) {
+            
+            //[self.locationManager startRangingBeaconsInRegion:beaconRegion];
+            //NSLog(@"Starting to range beacons");
+            
+            [self inside];
+        }
+    }
+}
+
+-(void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
+    NSLog(@"Did exit region");
+    
+    if ([region isKindOfClass:[CLBeaconRegion class]]) {
+        CLBeaconRegion *beaconRegion = (CLBeaconRegion *)region;
+        if ([beaconRegion.identifier isEqualToString:@"com.donwb.beacon.region"]) {
+            
+            //[self.locationManager stopRangingBeaconsInRegion:beaconRegion];
+            
+            [self outside];
+        }
+    }
+}
+/*
+-(void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
+    for(CLBeacon *beacon in beacons) {
+        NSLog(@"Ranging beacon: %@", beacon.proximityUUID);
+        NSLog(@"%@ - %@", beacon.major, beacon.minor);
+        NSLog(@"Range: %ld", beacon.proximity);
+    }
+}
+*/
+
+-(void) inside {
+    //self.view.backgroundColor = [UIColor greenColor];
+    UILocalNotification *notify = [UILocalNotification new];
+    notify.alertBody = @"Welcome!";
+    
+    [[UIApplication sharedApplication] presentLocalNotificationNow:notify];
+    
+    NSLog(@"notification sent...");
+    
+    //[self changeUIColor:true];
+    
+    [UIView animateWithDuration:0.5 delay:0.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         self.locationContainer.alpha = 0.0;
+                         
+                         CGRect newFrame = self.avatarImageView.frame;
+                         newFrame.size.width = 150;
+                         newFrame.size.height = 150;
+                         newFrame.origin.x = self.avatarImageView.frame.origin.x - 20.0;
+                         
+                         [self.avatarImageView setFrame:newFrame];
+                         
+                         
+                         [self changeUIColor:true];
+                     }
+                     completion:^ (BOOL finished){
+                         NSLog(@"done!");
+                         [self roundAvatar:150];
+                     }];
+
+}
+
+-(void) outside {
+    
+    UILocalNotification *notify = [UILocalNotification new];
+    notify.alertBody = @"Later!";
+    
+    [[UIApplication sharedApplication] presentLocalNotificationNow:notify];
+    
+    NSLog(@"notification sent...");
+    
+    [UIView animateWithDuration:0.5 delay:0.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         self.locationContainer.alpha = 1.0;
+                         [self.avatarImageView setFrame:self.initialAvatarFrame];
+                         
+                         [self changeUIColor:false];
+                     }
+                     completion:^ (BOOL finished){
+                         NSLog(@"done!");
+                         [self roundAvatar:100.0];
+                     }];
+    
+
+}
+
+#pragma mark UI Goop
+
+- (void)roundAvatar:(CGFloat) avatarSize
+{
+    self.avatarImageView.layer.cornerRadius = avatarSize/2;
+    self.avatarImageView.layer.borderWidth = 2.0f;
+    self.avatarImageView.clipsToBounds = YES;
+}
+
+- (IBAction)saveButton:(id)sender {
+    
+}
+- (IBAction)testButton:(id)sender {
+    NSLog(@"here");
+}
+
+-(void)changeUIColor:(BOOL)inside {
+    if(inside) {
+        self.view.backgroundColor = [UIColor colorWithRed:0.792 green:0.835 blue:0.706 alpha:1];
+        self.avatarImageView.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.locationContainer.backgroundColor = [UIColor colorWithRed:0.792 green:0.835 blue:0.706 alpha:1];
+    } else {
+        self.view.backgroundColor = [UIColor colorWithRed:0.918 green:0.659 blue:0.663 alpha:1];
+        self.locationContainer.backgroundColor = [UIColor colorWithRed:0.918 green:0.659 blue:0.663 alpha:1];
+        
+        self.avatarImageView.layer.borderColor = [UIColor colorWithRed:0.671 green:0.875 blue:0.973 alpha:1].CGColor;
+
+    }
+}
+
+
+
+#pragma mark test buttons
+
+- (IBAction)buttonPress:(id)sender {
+    [self inside];
+}
+
+
+- (IBAction)exitButtonPress:(id)sender {
+    [self outside];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +217,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
